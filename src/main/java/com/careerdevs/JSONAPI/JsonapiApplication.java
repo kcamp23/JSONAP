0426b1f617f15ac32ba3c19dd4b80c7898fa3564
Kat Camp <kcamp552369@gmail.com>
2022-03-09 13:02:24 -0500

    Initial commit

---
+++ src/main/java/com/careerdevs/JSONAPI/JsonapiApplication.java
@@ -0,0 +1,13 @@
+package com.careerdevs.JSONAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@RestController
+@SpringBootApplication
+public class JsonapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsonapiApplication.class, args);
+	}
+
+}
